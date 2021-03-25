02389c5a2c53b4690f20c0ef8c10199c7c2adcc0
MoyiLiu <moyi_liu@hotmail.com>
2021-03-25 22:01:32 +0000

    Gravity sensor & adapter (#2)
    * update gitignore
    
    * update gradle and add rxjava
    
    * fix rxjava android version
    
    * Implement GravitySensor

---
+++ app/src/main/java/com/moyi/liu/audiofeedback/sensor/GravitySensor.kt
@@ -0,0 +1,15 @@
+package com.moyi.liu.audiofeedback.sensor
+
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.subjects.PublishSubject
+
+interface GravitySensor {
+    val sensorDataStream: PublishSubject<Triple<Float, Float, Float>>
+
+    fun initialiseSensor(): Completable
+    fun register(): Completable
+    fun unregister()
+}
+
+object SensorInitialisationFailedException: Exception()
+object SensorNotFoundException: Exception()
