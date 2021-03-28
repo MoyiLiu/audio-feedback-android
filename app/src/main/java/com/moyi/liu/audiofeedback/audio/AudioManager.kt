ec4de06374718a78c5f72278e8ba6c5574c81429
Moyi Liu <moyi_liu@hotmail.com>
2021-03-28 20:01:06 +0100

    Introduce play rate control on front-back tracks

--- app/src/main/java/com/moyi/liu/audiofeedback/audio/AudioManager.kt
+++ app/src/main/java/com/moyi/liu/audiofeedback/audio/AudioManager.kt
@@ -8,10 +8,10 @@ interface AudioManager {
 
     fun loadSoundTracks()
     fun startLoopingTracksWithNoVolume()
-    fun updateFrontBackTracks(audioStreamContext: AudioStreamContext)
+    fun updateFrontBackTracks(audioStreamContexts: Pair<AudioStreamContext, AudioStreamContext>)
 }
 
-class AFAudioManager (private val ctx: Context) : AudioManager {
+class AFAudioManager(private val ctx: Context) : AudioManager {
 
     private var soundPool: SoundPool? = null
     private var frontTrackId: Int = 0
@@ -20,21 +20,24 @@ class AFAudioManager (private val ctx: Context) : AudioManager {
     override fun loadSoundTracks() {
         soundPool = SoundPool.Builder().setMaxStreams(MAX_STREAMS).build()
         frontTrackId = soundPool?.load(ctx, R.raw.guitar_triplet_asc, 1) ?: 0
-        backTrackId = soundPool?.load(ctx, R.raw.clarinet_c4 ,1) ?: 0
+        backTrackId = soundPool?.load(ctx, R.raw.clarinet_c4, 1) ?: 0
     }
 
     override fun startLoopingTracksWithNoVolume() {
         soundPool?.play(frontTrackId, 0f, 0f, 1, -1, 1f)
-        soundPool?.play(backTrackId, 0f,0f, 1, -1, 1f)
+        soundPool?.play(backTrackId, 0f, 0f, 1, -1, 1f)
     }
 
-    override fun updateFrontBackTracks(audioStreamContext: AudioStreamContext) {
-        soundPool?.setVolume(frontTrackId, audioStreamContext.frontVolume, audioStreamContext.frontVolume)
-        soundPool?.setVolume(backTrackId, audioStreamContext.backVolume, audioStreamContext.backVolume)
+    override fun updateFrontBackTracks(audioStreamContexts: Pair<AudioStreamContext, AudioStreamContext>) {
+        val (front, back) = audioStreamContexts
+        soundPool?.setVolume(frontTrackId, front.volume, front.volume)
+        soundPool?.setVolume(backTrackId, back.volume, back.volume)
 
+        soundPool?.setRate(frontTrackId, front.playRate)
+        soundPool?.setRate(backTrackId, back.playRate)
     }
 
-    companion object{
+    companion object {
         const val MAX_STREAMS = 3
     }
 
