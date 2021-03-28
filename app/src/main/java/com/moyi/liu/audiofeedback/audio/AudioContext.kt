fb7413ea76c35c5cdcf0089c8b02e4949521834d
Moyi Liu <moyi_liu@hotmail.com>
2021-03-28 22:07:56 +0100

    Implement SensorDataTransformer

--- app/src/main/java/com/moyi/liu/audiofeedback/audio/AudioContext.kt
+++ app/src/main/java/com/moyi/liu/audiofeedback/audio/AudioContext.kt
@@ -1,5 +1,8 @@
 package com.moyi.liu.audiofeedback.audio
 
+import com.moyi.liu.audiofeedback.transformer.MIN_VOLUME
+import com.moyi.liu.audiofeedback.transformer.NORMAL_PLAY_RATE
+
 /**
  * @param volume range 0.0 to 1.0
  * @param playRate range 1.0 to 2.0
@@ -7,7 +10,11 @@ package com.moyi.liu.audiofeedback.audio
 data class AudioContext(
     val volume: Float,
     val playRate: Float
-)
+) {
+    companion object {
+        val MUTE = AudioContext(MIN_VOLUME, NORMAL_PLAY_RATE)
+    }
+}
 
 
 
--- app/src/main/java/com/moyi/liu/audiofeedback/transformer/SensorDataTransformer.kt
+++ app/src/main/java/com/moyi/liu/audiofeedback/transformer/SensorDataTransformer.kt
@@ -4,6 +4,28 @@ import com.moyi.liu.audiofeedback.audio.AudioContext
 import com.moyi.liu.audiofeedback.sway.Boundary
 import kotlin.math.pow
 
+class SensorDataTransformer(
+    private val frontBackAxisInitialValue: Float,
+    private val frontBackBoundaries: Pair<Boundary, Boundary> //<Front, Back>
+) {
+    /**
+     * @param sensorData x,y,z axes values, refer to [https://developer.android.com/reference/android/hardware/SensorEvent]
+     * @return a pair of Font and Back [AudioContext]
+     */
+    fun transformForFrontBackTracks(sensorData: Triple<Float, Float, Float>): Pair<AudioContext, AudioContext> {
+        val (_, _, value) = sensorData
+        val (frontBoundary, backBoundary) = frontBackBoundaries
+        // value > frontBackAxisInitialValue ==> back
+        // value <= frontBackAxisInitialValue ==> front
+        return when {
+            value > frontBackAxisInitialValue ->
+                AudioContext.MUTE to value.transformToPlayRateAudioContext(backBoundary)
+            else ->
+                value.transformToLoudnessAudioContext(frontBoundary) to AudioContext.MUTE
+        }
+    }
+}
+
 fun Float.transformToLoudnessAudioContext(boundary: Boundary): AudioContext {
     val (min, max) = boundary
     return when {
