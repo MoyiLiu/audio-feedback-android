5eacb8c7d96a66f81099fef66d997d4d7f125495
MoyiLiu <moyi_liu@hotmail.com>
2021-03-28 20:07:21 +0100

    Audio manager (#3)
    * Add front/back sound files
    
    * Implement AudioManager to integrate with SoundPool
    
    * Introduce play rate control on front-back tracks
    
    Co-authored-by: RUI HUAN <ruihuan0329@gmail.com>

---
+++ app/src/main/java/com/moyi/liu/audiofeedback/audio/AudioStreamContext.kt
@@ -0,0 +1,13 @@
+package com.moyi.liu.audiofeedback.audio
+
+/**
+ * @param volume range 0.0 to 1.0
+ * @param playRate range 1.0 to 2.0
+ */
+data class AudioStreamContext(
+    val volume: Float,
+    val playRate: Float
+)
+
+
+
