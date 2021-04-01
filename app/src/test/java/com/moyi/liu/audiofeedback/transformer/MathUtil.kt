55f06bdfd41f6f1ed77921698b3a432fc0555a61
Moyi Liu <moyi_liu@hotmail.com>
2021-04-01 23:37:09 +0100

    Update comment

--- app/src/test/java/com/moyi/liu/audiofeedback/transformer/MathUtil.kt
+++ app/src/test/java/com/moyi/liu/audiofeedback/transformer/MathUtil.kt
@@ -2,6 +2,6 @@ package com.moyi.liu.audiofeedback.transformer
 
 import java.text.DecimalFormat
 
-//round the number up to integer to avoid floating point
+//round the number up to two decimals to avoid floating point
 fun Float.roundToTwoDecimals(): Float =
     DecimalFormat("#.##").format(this).toFloat()
